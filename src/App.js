911fa6b3f254cf55e80d7c2b8cec0f3d3680a6fa
parthiv360 <parthivsarkar360@gmail.com>
2021-10-04 01:13:11 +0530

    first app , class component

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/menucomponents';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div >
+         <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">
+                  Restaurant
+              </NavbarBrand>
+            </div>
+         </Navbar>
+         <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
